44a06bfd7d78eba1b8f9ec59e681e68534589c4e
VovanSuper <ovsyukov@yandex.com>
2022-04-07 22:09:03 +0300

    wip: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,49 +1,159 @@
-import { CarouselModern } from '../lib/index.js';
+import {
+  CarouselModern,
+  ContainerClassName,
+  getChildrenArray,
+  WrapperClassName,
+} from '../lib/index.js';
+import { setupTestCarousel, createTestStyles } from './helpers/utils.js';
 
 describe('All tests', () => {
+  const querySelectorStr = '#carousel';
+  /** @type {CarouselModern} */ let carousel;
   beforeEach(() => {
-    document.body.innerHTML = `
-  <div id="carousel" class="carousel-container">
-  <div class="slide-1"><img src="assets/1.jpeg" alt="Slide-1" class="slide-img-1" /></div>
-  <div class="slide-2"><img src="assets/2.jpeg" alt="Slide-2" class="slide-img-2" /></div>
-  <div class="slide-3"><img src="assets/3.jpeg" alt="Slide-3" class="slide-img-3" /></div>
-  <div class="slide-4"><img src="assets/4.jpeg" alt="Slide-4" class="slide-img-4" /></div>
-  <div class="slide-5"><img src="assets/5.jpeg" alt="Slide-5" class="slide-img-5" /></div>
-  <div class="slide-6"><img src="assets/6.jpeg" alt="Slide-6" class="slide-img-6" /></div>
-  <div class="slide-7"><img src="assets/7.jpeg" alt="Slide-7" class="slide-img-7" /></div>
-  <div class="slide-8"><img src="assets/8.jpeg" alt="Slide-8" class="slide-img-8" /></div>
-  <div class="slide-9"><img src="assets/9.jpeg" alt="Slide-9" class="slide-img-9" /></div>
-  </div>
-  `;
+    document.body.innerHTML = setupTestCarousel();
+    carousel = new CarouselModern(querySelectorStr);
   });
 
   describe('creation', () => {
     test('CarouselModule should be instantiated from querySelector', () => {
-      const querySelectorStr = '#carousel';
-      const carousel = new CarouselModern(querySelectorStr);
       expect(carousel).toBeInstanceOf(CarouselModern);
     });
     test('CarouselModule should be instantiated from html element', () => {
-      const containerEl = document.querySelector('#carousel');
+      const containerEl = document.querySelector(querySelectorStr);
       const carousel = new CarouselModern(containerEl);
       expect(carousel).toBeInstanceOf(CarouselModern);
     });
   });
 
-  describe('inner logic', () => {
-    /** @type {CarouselModern} carousel */ let carousel;
+  describe('carousel logic', () => {
+    test('CarouselModule should find all slides', () => {
+      const slides = carousel.slides;
+      expect(slides).toBeTruthy();
+      expect(slides).toBeInstanceOf(Array);
+      expect(carousel.slides).toBeInstanceOf(Array);
+    });
+
+    test('carousel has wrapper created', () => {
+      const wrapperEl = carousel.containerEl.querySelector(`.${WrapperClassName}`);
+
+      expect(wrapperEl).toBeInstanceOf(HTMLDivElement);
+      expect(wrapperEl.classList).toContain(WrapperClassName);
+
+      const slidesEls = getChildrenArray(wrapperEl);
+      expect(slidesEls).toHaveLength(9);
+    });
+
+    test('carousel slides elements', () => {
+      const newCarouselContainer = carousel.containerEl.cloneNode(true);
+
+      expect(newCarouselContainer).toBeInstanceOf(HTMLDivElement);
+      expect(getChildrenArray(newCarouselContainer.firstChild)).toBeArrayOfSize(9);
+
+      getChildrenArray(carousel.containerEl.firstChild)
+        .map((/** @type {HTMLElement} slide */ slide) => slide.className)
+        .forEach((className) => expect(className).toContain('slide'));
+
+      getChildrenArray(carousel.containerEl.firstChild)
+        .map((slide) => slide.firstChild)
+        .map((imgEl) => imgEl.className)
+        .forEach((className, i) =>
+          expect(className).toEqualCaseInsensitive(`slide-img-${i + 1}`)
+        );
+    });
+
+    test('moveToIndex() should set index', () => {
+      expect(carousel.currentIndex).toBe(0);
+
+      carousel.moveToIndex(5);
+      expect(carousel.currentIndex).toEqual(5);
+    });
+
+    test('subscriber invoked on carousel action', () => {
+      const mockSub = jest.fn((args) => args);
+      carousel.subscribeToSate(mockSub);
+      carousel.moveToNext();
+      carousel.moveToNext();
+      expect(mockSub).toBeCalledTimes(2);
+      expect(mockSub).toReturn();
+      expect(mockSub).toReturnTimes(2);
+    });
+
+    test('index should update on moveToNext / moveToPrev', () => {
+      //initially index should be '0';
+      expect(carousel.currentIndex).toEqual(0);
+
+      // move to next
+      carousel.moveToNext();
+      expect(carousel.currentIndex).toEqual(1);
+
+      // move to next once more
+      carousel.moveToNext();
+      expect(carousel.currentIndex).toEqual(2);
+
+      // now, move back
+      carousel.moveToPrev();
+      expect(carousel.currentIndex).toEqual(1);
+
+      // now, move back once more
+      carousel.moveToPrev();
+      expect(carousel.currentIndex).toEqual(0);
+
+      // now, move back once more
+      carousel.moveToPrev();
+      expect(carousel.currentIndex).toEqual(8);
+    });
+  });
+
+  describe('UI / styling', () => {
+    /** @type {HTMLDivElement} */ let wrapperEl;
+    let acceptedStylesheet;
+    let firstStylesSet;
 
     beforeEach(() => {
-      const containerEl = document.querySelector('#carousel');
-      carousel = new CarouselModern(containerEl);
+      document.adoptedStyleSheets = [
+        ...createTestStyles(`#carousel`, `.${WrapperClassName}`, `.slide`),
+      ];
+      wrapperEl = carousel.containerEl.querySelector(`.${WrapperClassName}`);
+      acceptedStylesheet = document.adoptedStyleSheets;
+      firstStylesSet = acceptedStylesheet[0];
     });
 
-    test('CarouselModule should find all slides', () => {
-      const slides = carousel.getSlides();
-      expect(slides).toBeTruthy();
-      expect(slides).toBeInstanceOf(NodeList);
-      expect(carousel.slides).toBeInstanceOf(NodeList);
-      console.log(carousel.slides)
+    //-- adopted styles --//
+    test('accordion wrapper has adopted styles applied', () => {
+      const slidesEls = wrapperEl.querySelectorAll('.slide');
+      const carouselContainerRules = firstStylesSet.cssRules[0];
+
+      const { selectorText } = carouselContainerRules;
+      const { width, height, overflow, 'box-sizing': Bsz } = carouselContainerRules.style; // #carousel;
+      expect(slidesEls.item(0)).toBeInstanceOf(HTMLDivElement);
+      expect(slidesEls).toHaveLength(9);
+      expect(carouselContainerRules.length).not.toEqual(0);
+      expect(height).toEqual('500px');
+      expect(width).toEqual('500px');
+      expect(overflow).toEqual('hidden');
+      expect(Bsz).toEqual('border-box');
+      expect(selectorText).toEqual(querySelectorStr);
     });
+
+    test('slide has test styles applied', () => {
+      const slidesRules = firstStylesSet.cssRules[2];
+      const { float, height, 'background-size': BgSize } = slidesRules.style; // #carousel;
+      const { selectorText } = slidesRules;
+
+      expect(selectorText).toBe('.slide');
+      expect(float).toEqual('left');
+      expect(height).toEqual('100%');
+      expect(BgSize).toEqual('cover');
+    });
+    //-- adopted styles end --//
+
+    //-- translate styles --//
+    test('should apply translate transform to carousel wrapper on moveToNext', () => {
+      carousel.moveToNext();
+      const { transform, transition, translate } = wrapperEl.style;
+
+      expect(translate).toContain('500px');
+    });
+    //-- translate styles end--//
   });
 });
